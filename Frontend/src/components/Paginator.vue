c3f62adba1275a74436ccaeeef8c8a52778fcf29
Blady7 <petihuawei2@gmail.com>
2024-12-12 12:27:14 +0100

    kártyák kész

---
+++ Frontend/src/components/Paginator.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item" v-for="page in pagesArray" :key="page">
+        <a class="page-link" href="#">{{ page }}</a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+</nav>
+  </div>
+</template>
+
+<script>
+
+export default {
+    props: ["pagesArray"],
+}
+</script>
+
+<style>
+
+</style>
--- Frontend/src/views/KartyakView.vue
+++ Frontend/src/views/KartyakView.vue
@@ -1,13 +1,65 @@
 <template>
-  <h1> Kártyák</h1>
+  <div>
+    <!-- fej -->
+    <h1>Kártyák</h1>
+
+    <!-- Kártyák -->
+    <div>
+      <Cards :cards="cards" />
+    </div>
+
+    <!-- paginátor -->
+    <div>
+      <Paginator :pagesArray="pagesArray" />
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import Cards from "../components/Cards.vue";
+import Paginator from "@/components/Paginator.vue";
 export default {
+  components: {
+    Cards,
+    Paginator,
+  },
+  data() {
+    return {
+      cards: [],
+      url: "http://localhost:8000/api",
+      currentPage: 2, //melyik oldalon vagyunk
+      cardsInPage: 3, //hány kártya lehet egy oldalon
+      pagesArray: [], //az oldalak számok tömbje
+      pagesNumber: 1, // összesen hány oldalunk van
+    };
+  },
+  mounted() {
+    this.getCards();
+    this.getPagesNumber();
+  },
+  methods: {
+    async getCards() {
+      const url = `${this.url}/queryOsztalynevsorLimit/${this.currentPage}/${this.cardsInPage}`;
+      const response = await axios.get(url);
+      this.cards = response.data.data;
+      console.log(this.cards);
+    },
 
-}
+    async getPagesNumber() {
+      const url = `${this.url}/queryHanyOldalVan/${this.cardsInPage}`;
+      const response = await axios.get(url);
+      this.pagesNumber = response.data.data.oldalszam;
+      console.log(this.pagesNumber);
+      this.pagesArray = [];
+      for (let i = 0; i < this.pagesNumber; i++) {
+        this.pagesArray.push(i + 1)
+        
+      }
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
